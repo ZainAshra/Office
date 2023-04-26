b6ac4da30f5e598e2cc6970c87cbb5ae82debe54
JS Intern1 <JsIntern1@plentyz.com.pk>
2023-04-26 18:38:53 +0500

    office

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Search from '../components/search'
+// import Search from './search'
+
+export default function Header() {
+  return (
+   <>
+    <div className='header'>
+<div >
+  <Search/>
+</div>
+    </div>
+
+   </>
+  )
+}
---
+++ src/components/search.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import searchIcon from "../images/Icon (Stroke).png";
+import crossIcon from "../images/crossIcon.png";
+import "./Search.css";
+
+export default function Search() {
+  const [showValue, setshowValue] = useState(false);
+
+  const setValue = (e) => {
+    
+    setshowValue(e)
+    console.log(showValue)
+  };
+  const clearField = () => {
+    console.log("run");
+    setshowValue(false);
+    console.log(showValue)
+  };
+  return (
+    <div className="search-container">
+      <div className="search-wrapper">
+        <img src={searchIcon} alt="search" className="search-icon" />
+        <input
+          type="text"
+          placeholder="What are you looking for?"
+          className="search-bar"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <img
+          src={crossIcon}
+          alt="search"
+          className="cross-icon"
+          onClick={() => {
+            clearField();
+          }}
+        />
+      </div>
+    </div>
+  );
+}
