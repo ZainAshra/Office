ae8e4c1133533b3b761f2f3c40b53e6f0e22d503
JS Intern1 <JsIntern1@plentyz.com.pk>
2023-04-29 18:41:02 +0500

    4day

--- src/components/header.js
+++ src/components/header.js
@@ -5,78 +5,81 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import drawer from "../images/drawer.png";
 import companylogo from "../images/company_logo.png";
 import "./header.css";
-import rightIcon from '../images/Icon.png'
-import axios from 'axios';
-import { Category, CleaningServices } from "@mui/icons-material";
+import rightIcon from "../images/Icon.png";
+import axios from "axios";
+import _ from 'lodash';
 export default function Header() {
+  const [data, setData] = useState([]);
+  const [parentData, setParentData] = useState([]);
 
+  const chk = [];
 
-const [data ,setData]=useState('');
+  useEffect(() => {
+    axios
+      .get("https://api.plentys.pk/api/v1/public/allCategories?cityId=1")
 
+      .then((response) => {
+        console.log(response.data.data);
+        // setData(response.data.data);
+        console.log(response.data.data.length);
 
-useEffect(()=>{
-axios.get('https://api.plentys.pk/api/v1/public/allCategories?cityId=1')
+        for (let i = 0; i < response.data.data.length; i++) {
+          console.log("run");
 
-.then(response=>{
-   console.log(response.data)
-  setData(response.data)
+          chk.push(response.data.data[i]);
+          setData(chk);
+          // console.log(chk[i].parentId,'array')
+        }
+      })
+      .catch((error) => {
+        console.log(error, "api error");
+      });
+  }, []);
 
-})
-.catch(error=>{
-console.log(error,'api error')
-})
+  // const catogries = [
+  //   "Plentysmart",
+  //   "Beauty & Grooming",
+  //   "Home & Lifestyle",
+  //   "Women’s Fashion",
+  //   "Men’s Fashion",
+  //   "Health & Sports",
+  //   "Mobile & Tablet",
+  //   "Computing",
+  //   "Flash Deals",
+  // ];
 
+  // console.log(data);
+  const groups = _.groupBy(data, 'parentId');
+  // console.log(groups)
 
-
-},[])
-
-
-  const catogries = [
-    "Plentysmart",
-    "Beauty & Grooming",
-    "Home & Lifestyle",
-    "Women’s Fashion",
-    "Men’s Fashion",
-    "Health & Sports",
-    "Mobile & Tablet",
-    "Computing",
-    "Flash Deals",
-  ];
-  const cat=data.data;
-
-  cat.forEach((element)=>{
-    console.log(element.name)
+  const arr = _.map(_.toPairs(groups), ([key, value]) => ({ key, value }));
+// console.log(arr)
+  arr.map((x,i)=>{
+    return(console.log(x.value,i))
   })
-
   return (
     <>
+      <div>
+        <img src={drawer} alt="" />
 
-
-
-
-<div>
- {/* {cat.map((e,i)=>{
-return(<p>{e.name}</p>)
- })} */}
-</div>
-   
-<div>
-
-
-<img src={drawer} alt=""  />
-
-          <div class="container" className="conatiner">
+        <div class="container" className="conatiner">
           <h1>Catgories</h1>
-          {catogries.map((item,index)=>{return <div key={index} class='row' className="row">
-              <span >{item} <span><img src={rightIcon} alt='icon'/></span></span>
-            </div>
-})}
-           
-          </div>
+          {data.map((item, index) => {
+            return (
+              <div key={index} class="row" className="row">
+                <span>
+                  {item.name}
+                  <span>
+                    <img src={rightIcon} alt="icon" />
+                  </span>
+                </span>
+              </div>
+            );
+          })}
         </div>
+      </div>
 
-{/* {console.log(data)} */}
-
-      </>
+      {/* {console.log(data)} */}
+    </>
   );
 }
